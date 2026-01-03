a34886a7364b0975327e7531d0c4c91914d55f0e
lanaelhajj-2905 <lanaelhajj92120@gmail.com>
2026-01-03 12:01:45 +0100

    changes init

--- src/strategy/data/__init__.py
+++ src/strategy/data/__init__.py
@@ -1,7 +1,8 @@
 """Module de chargement et transformation de données."""
 
 from src.strategy.data.loaders import load_single, load_multiple
-from src.strategy.data.transforms import log_returns, log_returns_df, split_data
+from src.strategy.data.transforms import compute_log_returns as log_returns, compute_log_returns_df as log_returns_df, split_train_val_test as split_data
+
 
 __all__ = [
     "load_single",
